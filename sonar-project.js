26fea8e6ccba885b252779276ec9791cd9cb252a
ecomlisters <105581856+ecomlisters@users.noreply.github.com>
2023-07-29 15:26:38 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,7 @@ scanner(
     serverUrl : 'http://localhost:9000/',
     token : "sqp_7c6df26ff26031fa62f4c57849b23896bb7291cc",
     options: {
-      'sonar.projectName': 'SonarProject',
+      'sonar.projectName': 'TestProject',
       'sonar.projectDescription': 'Description for "My App" project...',
       'sonar.sources': '.',
       'sonar.language': 'js'
