ff977c921b9b7390c00813b5a8d8bd27baf0c323
ecomlisters <105581856+ecomlisters@users.noreply.github.com>
2023-07-29 12:48:17 +0530

    Create sonar-project.js

---
+++ sonar-project.js
@@ -0,0 +1,15 @@
+const scanner = require('sonarqube-scanner');
+
+scanner(
+  {
+    serverUrl : 'http://localhost:9000/',
+    token : "sqp_7c6df26ff26031fa62f4c57849b23896bb7291cc",
+    options: {
+      'sonar.projectName': 'TestProject',
+      'sonar.projectDescription': 'Description for "My App" project...',
+      'sonar.sources': '.',
+      'sonar.language': 'js'
+    }
+  },
+  () => process.exit()
+)
